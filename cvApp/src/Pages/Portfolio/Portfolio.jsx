b56433fc67fb3ba4db1196c7f8cd003418bfda56
TheJahanzaibLiaqat <jahan.zaib3373@gmail.com>
2024-07-15 06:44:09 -0700

    hmm

--- cvApp/src/Pages/Portfolio/Portfolio.jsx
+++ cvApp/src/Pages/Portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@
 import { FaCirclePlus } from "react-icons/fa6";
 const Portfolio = () => {
   const values = [
-    { img: "images/my.jpeg", component: FaCirclePlus, title: "Medical", description: "Website", url:"https://github.com/TheJahanzaibLiaqat/ClaueEcommerce_React" },
+    { img: "images/WEb.PNG", component: FaCirclePlus, title: "Medical", description: "Website", url:"https://github.com/TheJahanzaibLiaqat/ClaueEcommerce_React" },
     { img: "images/CodeStore.PNG", component: FaCirclePlus, title: "CodeStore", description: "Website" , url:"https://github.com/TheJahanzaibLiaqat/CodeStore_Website_React" },
     { img: "images/Ecommerce.PNG", component: FaCirclePlus, title: "Ecommerce", description: "Website" , url:"https://github.com/TheJahanzaibLiaqat/EcommerceApp_React"},
     { img: "images/Movie.PNG", component: FaCirclePlus, title: "Movie", description: "Website", url:"https://github.com/TheJahanzaibLiaqat/MovieApp_React/tree/main/movieApp" },
